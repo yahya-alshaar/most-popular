7c9e1e41a522b885156823eb8e5d9c61a6b15470
Yahya Alshaar <yalshaar@justclean.com>
2019-05-13 13:23:51 +0300

    add pods and debugger

---
+++ Pods/Target Support Files/Pods-PopularArticles/Pods-PopularArticles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PopularArticles : NSObject
+@end
+@implementation PodsDummy_Pods_PopularArticles
+@end
---
+++ Pods/Target Support Files/Pods-PopularArticlesTests/Pods-PopularArticlesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PopularArticlesTests : NSObject
+@end
+@implementation PodsDummy_Pods_PopularArticlesTests
+@end
